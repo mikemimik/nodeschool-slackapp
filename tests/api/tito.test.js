255d9c0705c540fba7198d223765ce29ad936bf3
Michael Perrotte <michael.perrotte@resolver.com>
2017-07-22 21:25:09 -0400

    add initial api test file

--- tests/api/tito.test.js
+++ tests/api/tito.test.js
@@ -1,24 +1,70 @@
 'use strict';
 
 const proxyquire = require('proxyquire').noCallThru();
+const expect = require('chai').expect;
 const sinon = require('sinon');
+const _ = require('lodash');
 
 describe('TITO API', function() {
-    let requestStub;
+  describe('Module Exports', function() {
     let titoApi;
+    let fixtures;
 
     beforeEach(function() {
-        requestStub = sinon.stub().resolves({});
-        titoApi = proxyquire('../../api/tito', {
-            'request-promise': requestStub
-        });
-    });
-    describe('Module Exports', function() {
-        describe('getLatestEvent', function() {
+      const requestStub = (options) => {
+        // console.log('options:', options);
+        console.log('fixtures:', fixtures);
 
-            return titoApi.getLatestEvent().then(event => {
-
-            });
-        });
+        return sinon.stub().resolves(fixtures)();
+      };
+      
+      titoApi = proxyquire('../../api/tito', {
+        'request-promise': requestStub
+      });
     });
+
+    describe('getLatestEvent', function() {
+      fixtures = {
+        data: [
+          {
+            id: 'event-name',
+            type: 'events',
+            attributes: { slug: 'event-name-slug', 'start-date': '2017-01-01' }
+          },
+          {
+            id: 'event-name',
+            type: 'events',
+            attributes: { slug: 'event-name-slug', 'start-date': '2016-01-01' }
+          },
+          {
+            id: 'event-name',
+            type: 'events',
+            attributes: { slug: 'event-name-slug', 'start-date': '2017-05-01' }
+          }
+        ]
+      };
+
+      it('Should return the lastest event', function() {
+        const expectedEvent = _.sortBy(
+          fixtures.data,
+          [
+            (event) => {
+              const date = event.attributes['start-date'];
+              const year = date.split('-')[0];
+              return year;
+            },
+            (event) => {
+              const date = event.attributes['start-date'];
+              const year = date.split('-')[1];
+              return year;
+            }
+          ]
+        ).pop();
+        return titoApi.getLatestEvent().then(event => {
+          expect(event).to.be.an('object');
+          expect(event.attributes['start-date']).to.be.deep.equal(expectedEvent.attributes['start-date']);
+        });
+      });
+    });
+  });
 });
