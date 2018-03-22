6c56c9117f8a013a02856a1e023aaea835f13548
Michael Perrotte <michael.perrotte@resolver.com>
2018-03-22 01:00:48 -0400

    linting index file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // APPLICATION FUNCTION
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,6 +16,6 @@ app.use('/checkin', require('./routes/checkin'));
 app.use('/links', require('./routes/links'));
 app.use('/register', require('./routes/register'));
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
